0ce015efe7d7c515a6d491676bdfb884ad8e0649
TechMama1 <carla1thurston@gmail.com>
2018-09-22 17:29:14 -0500

    wrapper css

--- src/components/FriendCard/FriendCard.js
+++ src/components/FriendCard/FriendCard.js
@@ -5,21 +5,7 @@ const FriendCard = props => (
   <div onClick={()=> props.selectCharacter(props.id) } className="card">
     <div className="img-container">
       <img alt={props.name} src={props.image} />
-    </div>
-    <div className="content">
-      <ul>
-        <li>
-          <strong>Name:</strong> {props.name}
-        </li>
-        <li>
-          <strong>Occupation:</strong> {props.occupation}
-        </li>
-        <li>
-          <strong>Location:</strong> {props.location}
-        </li>
-      </ul>
-    </div>
-   
+    </div> 
   </div>
 );
 
